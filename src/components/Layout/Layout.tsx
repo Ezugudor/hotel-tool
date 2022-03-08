d31452cd4a2831de32ce012ccaa8ea1a5d57f6a7
ezugudor <nelsonsmrt@gmail.com>
2022-03-08 20:30:16 +0100

    Implemented the TDD for App and Layout component with Context

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Dashboard, Sidebar } from "..";
+
+
+export const Layout = () =>
+    <div>
+        <Sidebar />
+        <Dashboard />
+    </div>;
---
+++ src/context/CustomerContext.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import { CustomerBids } from "../mocks/CustomerBids";
+
+export const CustomerContext = React.createContext({})
+
+export interface CustomerProviderProps {
+    children: ReactNode
+}
+
+export const CustomerProvider = (props: CustomerProviderProps) => {
+    return <CustomerContext.Provider value={CustomerBids}>{props.children}</CustomerContext.Provider>
+}
